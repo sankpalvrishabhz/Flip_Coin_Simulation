56d9f2cfd85da646715d7e460e6f1752077c4f24
sankpalvrishabhz <sankpalvrishabhz@gmail.com>
2020-07-10 13:26:28 +0530

    Multiple Fliping for a limit has been Added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,17 @@
 #!/bin/bash -x
-num=$((RANDOM%2))
-if [ $num -eq 0 ]
-then
-        echo "Heads"
-else
-        echo "Tails"
-fi
+read -p "How many times Do you Want to flip the Coin:" times
+while [[ $count -lt times ]]
+do
+        num=$((RANDOM%2))
+        if [[ $num -eq 0 ]]
+        then
+                echo "Heads"
+                ((count_H++))
+        else
+                echo "Tails"
+                ((count_T++))
+        fi
+	((count++))
+done
+echo -e "\nHeads have been Won $count_H times"
+echo -e "\nTails have been Won $count_T times"
